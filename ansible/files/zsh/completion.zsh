92767c3b1e2d7e5a10a1e806a6f517248d9ac039
Luizm <luizmuller.it@gmail.com>
2019-03-11 16:56:36 -0300

    add gcloud

--- ansible/files/zsh/completion.zsh
+++ ansible/files/zsh/completion.zsh
@@ -1,1 +1,3 @@
 source <(kubectl completion zsh)
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
