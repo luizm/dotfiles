8e8886870d2f19ce0e1f0a9bc85ddbebe1caea1a
Luizm <luizmuller.it@gmail.com>
2019-03-11 21:41:05 -0300

    fmt

--- ansible/files/zsh/functions.zsh
+++ ansible/files/zsh/functions.zsh
@@ -1,35 +1,39 @@
+ # shellcheck disable=SC2148
 pub_key() {
-    cat ~/.ssh/id_rsa.pub | pbcopy
+	cat ~/.ssh/id_rsa.pub | pbcopy
 }
 
-convbase64() { 
-    echo -n $1 | base64 | pbcopy 
+convbase64() {
+	echo -n $1 | base64 | pbcopy
 }
 
-decodebase64() { 
-    echo -n $1 | base64 --decode 
+decodebase64() {
+	echo -n $1 | base64 --decode
 }
 
-sub(){
-  grep -R -l $1 * | 
-    while read i; do 
-        sed -i "s/$1/$2/g" $i ; 
-    done
+sub() {
+	grep -R -l $1 * |
+		while read i; do
+			sed -i "s/$1/$2/g" $i
+		done
 }
 
-p(){
-  dig +time=1 www.google.com.br >/dev/null || { echo "DNS server is down" && return 1 }
-  ping 8.8.8.8
+p() {
+	dig +time=1 www.google.com.br >/dev/null || {
+		echo "DNS server is down" &&
+			return 1
+	}
+	ping 8.8.8.8
 }
 
-vault_login(){
-    domain="$1"
-    export VAULT_AUTH_GITHUB_TOKEN=$(cat ~/.github/token)
-    export VAULT_ADDR="https://$domain:8200"
-    vault auth -method=github
+vault_login() {
+	domain="$1"
+	export VAULT_AUTH_GITHUB_TOKEN=$(cat ~/.github/token)
+	export VAULT_ADDR="https://$domain:8200"
+	vault auth -method=github
 }
 
-gcloud_login(){
-    account="$1"
-    export GOOGLE_CREDENTIALS="/Users/luiz.muller/.gcloud/$account.json"
+gcloud_login() {
+	account="$1"
+	export GOOGLE_CREDENTIALS="/Users/luiz.muller/.gcloud/$account.json"
 }
