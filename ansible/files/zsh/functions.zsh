fe8eaac6da20f1d5bab7ea22ab2cd8c18f8e6952
Luizm <luizmuller.it@gmail.com>
2019-05-22 18:27:45 -0300

    update functions

--- ansible/files/zsh/functions.zsh
+++ ansible/files/zsh/functions.zsh
@@ -1,20 +1,20 @@
  # shellcheck disable=SC2148
 pub_key() {
-	cat ~/.ssh/id_rsa.pub | pbcopy
+	pbcopy < "$HOME"/.ssh/id_rsa.pub
 }
 
 convbase64() {
-	echo -n $1 | base64 | pbcopy
+	echo -n "$1" | base64 | pbcopy
 }
 
 decodebase64() {
-	echo -n $1 | base64 --decode
+	echo -n "$1" | base64 --decode
 }
 
 sub() {
-	grep -R -l $1 * |
-		while read i; do
-			sed -i "s/$1/$2/g" $i
+	grep -R -l "$1" * |
+		while read -r i; do
+			sed -i "s/$1/$2/g" "$i"
 		done
 }
 
@@ -28,14 +28,16 @@ p() {
 
 vault_login() {
 	local -r env="$1"
+	local -r vault_addr="$(grep "$env" ~/.vault-config | cut -d: -f2-)"
 
-	export VAULT_ADDR="$(grep "$env" ~/.vault-config | cut -d: -f2-)"
+	export VAULT_ADDR="$vault_addr"
 	export VAULT_CACERT="$HOME/.cert/vault-$env.crt"
 
 	vault login -method=github token="$(cat ~/.github/token)"
 }
 
 gcloud_login() {
-	account="$1"
+	local -r account="$1"
+
 	export GOOGLE_CREDENTIALS="/Users/luiz.muller/.gcloud/$account.json"
 }
