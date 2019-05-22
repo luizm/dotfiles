e4d7f8774242a03f2fe77f5da250b59a2e4f4b08
Luizm <luizmuller.it@gmail.com>
2019-05-22 18:41:14 -0300

    add vault completion

--- ansible/files/zsh/completion.zsh
+++ ansible/files/zsh/completion.zsh
@@ -2,3 +2,5 @@
 source <(kubectl completion zsh)
 source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
 source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
+autoload -U +X bashcompinit && bashcompinit
+complete -o nospace -C /usr/local/bin/vault vault
